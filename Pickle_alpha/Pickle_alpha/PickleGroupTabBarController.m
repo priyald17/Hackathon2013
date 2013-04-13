61e8092d5276a1b87dd581cbe91b337f9b6d4cf1
Team 1 <claire.ma.murphy@gmail.com>
2013-04-13 01:25:12 -0700

    tab window - in progress

---
+++ Pickle_alpha/Pickle_alpha/PickleGroupTabBarController.m
@@ -0,0 +1,38 @@
+//
+//  PickleGroupTabBarController.m
+//  Pickle_alpha
+//
+//  Created by jarthur on 4/13/13.
+//  Copyright (c) 2013 com.hack2013. All rights reserved.
+//
+
+#import "PickleGroupTabBarController.h"
+
+@interface PickleGroupTabBarController ()
+
+@end
+
+@implementation PickleGroupTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
