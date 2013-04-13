a90963e8dbbc07bcec76ba24c949722a0f995e1b
Team 1 <claire.ma.murphy@gmail.com>
2013-04-13 02:16:26 -0700

    new pickle and new group

---
+++ Pickle_alpha/Pickle_alpha/NewPickleViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewPickleViewController.h
+//  Pickle_alpha
+//
+//  Created by jarthur on 4/13/13.
+//  Copyright (c) 2013 com.hack2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewPickleViewController : UIViewController
+
+@end
---
+++ Pickle_alpha/Pickle_alpha/NewPickleViewController.m
@@ -0,0 +1,38 @@
+//
+//  NewPickleViewController.m
+//  Pickle_alpha
+//
+//  Created by jarthur on 4/13/13.
+//  Copyright (c) 2013 com.hack2013. All rights reserved.
+//
+
+#import "NewPickleViewController.h"
+
+@interface NewPickleViewController ()
+
+@end
+
+@implementation NewPickleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
