36e74837401a983687e7fd58c724f6306a27a0db
Brian Hentschel <salmanakhtar@wl-27-8.cine.claremont.edu>
2013-04-13 04:38:10 -0700

    Brian Hentschel likes to push stuff

--- Pickle_alpha/Pickle_alpha/AppDelegate.h
+++ Pickle_alpha/Pickle_alpha/AppDelegate.h
@@ -12,4 +12,53 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+// the following methods get data from the current user
+
++(NSMutableArray*) getPickleMessages;
+
++(NSString*) getPickleIDForMessage:(NSString*)message;
+
++(NSMutableArray*) getGroupsForUser;
+
++(NSMutableArray*) getPickleesForGroupName:(NSString*)name;
+
++(NSMutableString*) getUsernameForCurrentUser;
+
++(NSMutableArray*) getFriendsNamesForCurrentUser;
+
++(NSMutableArray*) getFriendsIDForFriend:(NSString*)friendName;
+
+// the following methods are able to be called by a user to edit groups, pickles, etc.
+
++(BOOL)createGroup:(NSString*)groupName;
+
++(BOOL)createPickleWithQuestion:(NSString*)message andBeginTime:(NSDate*)startTime andEndTime:(NSDate*)endTime;
+
++(BOOL)addSuggestedPlace:(NSString*)place forPickle:(NSString*)pickleID;
+
++(BOOL)addSuggestedTime:(NSDate*)date forPickle:(NSString*)pickleID;
+
++(BOOL)addFriend:(NSString*)friendName forPickle:(NSString*)pickleID;
+
+// the following methods get data from a pickle. Note that each must be passed a pickle id.
+
++(NSString*)getPicklerIDForPickle:(NSString*)pickle;
+
++(NSMutableArray*)getPickleesNamesForPickle:(NSString*)pickle;
+
++(NSMutableArray*)getPickleesIDForPickle:(NSString*)pickle;
+
++(NSString*)getPickleMessageForID:(NSString*)pickle;
+
++(NSDate*)getBeginTimeRangeForPickle:(NSString*)pickle;
+
++(NSDate*)getEndTimeRangeForPickle:(NSString*)pickle;
+
+// will be a vector of NSStrings.
++(NSMutableArray*)getSuggestedPlacesForPickle:(NSString*)pickle;
+
+// will be a vector of NSDates.
++(NSMutableArray*)getSuggestedTimesForPickle:(NSString*)pickle;
+
+
 @end
