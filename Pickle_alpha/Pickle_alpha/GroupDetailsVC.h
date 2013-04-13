eb8e44c1926b2b13aad58c3f63479329768b4b9f
dgodevais <dustin.godevais@pomona.edu>
2013-04-13 04:20:30 -0700

    added GroupDetailsVC

---
+++ Pickle_alpha/Pickle_alpha/GroupDetailsVC.h
@@ -0,0 +1,13 @@
+//
+//  GroupDetailsVC.h
+//  Pickle_alpha
+//
+//  Created by Dustin Godevais on 4/13/13.
+//  Copyright (c) 2013 com.hack2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupDetailsVC : UIViewController
+
+@end
