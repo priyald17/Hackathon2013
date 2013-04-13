b6e3c6dab808f27f0a47ccb7770dad2033a44334
dgodevais <dustin.godevais@pomona.edu>
2013-04-13 05:04:46 -0700

    Hellow world

---
+++ Pickle_alpha/Pickle_alpha/PickleDetailsVC.m
@@ -0,0 +1,38 @@
+//
+//  PickleDetailsVC.m
+//  Pickle_alpha
+//
+//  Created by Dustin Godevais on 4/13/13.
+//  Copyright (c) 2013 com.hack2013. All rights reserved.
+//
+
+#import "PickleDetailsVC.h"
+
+@interface PickleDetailsVC ()
+
+@end
+
+@implementation PickleDetailsVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
